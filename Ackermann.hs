e8b637f0999c0869ee47f921e6619d7879eb5e88
Alberto <albamigh@gmail.com>
2023-03-22 22:30:04 +0100

    Restyled

--- Ackermann.hs
+++ Ackermann.hs
@@ -2,6 +2,6 @@ module Ackermann (ackermann) where
 
 ackermann :: Int -> Int -> Int
 ackermann m n
-    | m == 0 = n +1
-    | n == 0 = ackermann (m-1) 1
-    | otherwise =  ackermann m (ackermann m n-1)
+    | m == 0 = n + 1
+    | n == 0 = ackermann (m - 1) 1
+    | otherwise =  ackermann m (ackermann m n - 1)
