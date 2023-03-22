6d297776d11abf0013eec54cf4eaa11dc709eec7
Alberto <albamigh@gmail.com>
2023-03-22 22:04:30 +0100

    Ackermann working

---
+++ Ackermann.hs
@@ -0,0 +1,7 @@
+module Ackermann (ackermann) where
+
+ackermann :: Int -> Int -> Int
+ackermann m n
+    | m == 0 = n +1
+    | n == 0 = ackermann (m-1) 1
+    | otherwise =  ackermann m (ackermann m n-1)
